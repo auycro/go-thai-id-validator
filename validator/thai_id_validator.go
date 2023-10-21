732a33ebc888466b93ca45965dbbe2a47ec5969a
silversky <nopphadon.phanwong@gmail.com>
2023-10-21 11:28:49 +0700

    Refractor variable name in project.

--- validator/thai_id_validator.go
+++ validator/thai_id_validator.go
@@ -5,24 +5,24 @@ import (
 	"strconv"
 )
 
-func ThaiIDValidator(id string) bool {
-	if id == "" {
+func ThaiIDValidator(citizenID string) bool {
+	if citizenID == "" {
 		return false
 	}
 
 	r, _ := regexp.Compile("!/^[0-9]{13}$/g")
-	if r.MatchString(id) {
+	if r.MatchString(citizenID) {
 		return false
 	}
 
 	sum := 0
-	for i, n := range id[0 : len(id)-1] {
+	for i, n := range citizenID[0 : len(citizenID)-1] {
 		xint, _ := strconv.Atoi(string(n))
 		sum += xint * (13 - i)
 	}
 
 	checkSum := (11 - sum%11) % 10
-	last_num, _ := strconv.Atoi(id[len(id)-1:])
+	last_num, _ := strconv.Atoi(citizenID[len(citizenID)-1:])
 
 	return checkSum == last_num
 }
